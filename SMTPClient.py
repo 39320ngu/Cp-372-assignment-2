39f9d93ebb491c63785bda7328b4681ff719af5e
39320ngu <47791765+39320ngu@users.noreply.github.com>
2023-11-22 01:42:20 +0000

    added internal text that should be there for context

--- SMTPClient.py
+++ SMTPClient.py
@@ -12,6 +12,19 @@ def auth_plain(sender_email, password):
     return "\0%s\0%s" % (sender_email, password)
 
 
+def send_msg(message, expect_return_msg = True):
+    tcpSocket.send(f"{message}\r\n".encode())
+    if expect_return_msg:
+        recv = recieve_msg()
+        print(recv)
+        return recv  
+
+def recieve_msg():
+    try:
+        return tcpSocket.recv(2048).decode()
+    except timeout:
+        pass
+
 msg = "\r\n I love computer networks!"
 endmsg = "\r\n.\r\n"
 # Choose a mail server (e.g. Google mail server)
@@ -21,8 +34,15 @@ mailserver = 'smtp.gmail.com'
 # Make a TCP connection with mailserver and receive the server
 #response
 tcpSocket=socket(AF_INET, SOCK_STREAM)
+tcpSocket.connect(mailserver)
+
+tcpSocket.settimeout(10)
+
+
 #check the server response and print it to the screen
 
+
+recieve_msg()
 
 
 # Send HELO command to the server and print server response. It should be ehlo not helo
@@ -30,8 +50,12 @@ tcpSocket=socket(AF_INET, SOCK_STREAM)
 heloCommand = 'ehlo [' + addr +']\r\n'
 
 
+    
+send_msg(heloCommand, True)
+
 # Send STARTTLS command and print server response.
 starttlsCommand = 'STARTTLS\r\n'
+
 # wrap the socket you created earlier in a ssl context. Assuming you
 # named you socket, clientSocket, you can use the following two lines
 # to do so:
@@ -39,13 +63,41 @@ starttlsCommand = 'STARTTLS\r\n'
 
 
 context = ssl.create_default_context()
-clientSocket = context.wrap_socket(clientSocket, server_hostname=mailserver)
+tcpSocket = context.wrap_socket(tcpSocket, server_hostname=mailserver)
+
+"""
+Now, the client needs to authenticate to the server. The AUTH command is used for this
+purpose. The AUTH command sends the clients username and password to the e-mail server.
+AUTH can be combined with some other keywords such as PLAIN, LOGIN, CRAM-MD5 and
+DIGEST-MD5 (e.g. AUTH LOGIN) to choose an authentication mechanism. The authentication
+mechanism chooses how to login and which level of security that should be used. To
+authenticate using AUTH PLAIN you do so:
+
+Send the AUTH PLAIN command to the server.
+Command = ‘AUTH PLAIN\r\n’
+
+
+After the client has sent the AUTH PLAIN command to the server, the server responds with a
+334 reply code. Then the username and password are sent from the client to the server. The
+username and password are combined to one string and BASE64 encoded (using
+encode_base64 function in python). To combine username and password you can use the
+auth_plain function given above. Although the keyword PLAIN is used, the username and
+password are not sent as plain text over the Internet - they are always BASE64 encoded. If you
+want to read more about BASE64 encoding, look here.
+
+"""
+
 
 
 # Send DATA command and print server response.
 # Send message data.
 # Message ends with a single period.
 message = msg + endmsg
+
+
+
+
+
 # Send QUIT command and get server response.
 quitCommand = 'QUIT\r\n'
 
